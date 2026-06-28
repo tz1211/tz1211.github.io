5877eba1ebefa16c852e78be3065a2a5c881ee46
tz1211 <114442618+tz1211@users.noreply.github.com>
2026-06-28 00:21:29 +0000

    Deploying to gh-pages from @ tz1211/tz1211.github.io@44e730c0382f23ac1e632b70f477f3a1dd9f0ccf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "",
           section: "Navigation",
